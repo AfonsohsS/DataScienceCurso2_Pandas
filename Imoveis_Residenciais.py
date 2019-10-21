1532af7a36807ae339a3d25e2e276d07a61f9ce0
AfonsohsS <afonsohs@gmail.com>
2019-10-21 13:22:20 -0300

    Exportando o DataFrame para um arquivo csv

--- Imoveis_Residenciais.py
+++ Imoveis_Residenciais.py
@@ -33,3 +33,27 @@ dados_residencial.shape[0] # Devolve o tamanho do DataFrame
 dados_residencial.index = range(dados_residencial.shape[0])
 
 dados_residencial.head(20)
+
+# Solução do exercício de fixação
+# =====================================================
+
+numeros = [i for i in range(11)]
+letra = [chr(i + 65) for i in range(11)]
+nome_coluna = ['N']
+df_teste = pd.DataFrame(data = numeros, 
+                        index = letra, 
+                        columns = nome_coluna)
+df_teste
+
+selecao = df_teste.N.isin([i for i in range(11) if i % 2 == 0])
+
+selecao
+
+df_teste[selecao]
+# ====================================================
+
+# Exportando a Base de Dados Filtrada
+
+# Use o index = False para não exporta o index anterior para o novo arquivo
+dados_residencial.to_csv('aluguel_residencial.csv', sep = ';', 
+                                                    index = False)
