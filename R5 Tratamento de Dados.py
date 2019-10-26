90b6ac065890eb23ec32700e96e0c0653c276be0
AfonsohsS <afonsohs@gmail.com>
2019-10-25 23:32:02 -0300

    Filtrando e tratando valores nulos

--- R5 Tratamento de Dados.py
+++ R5 Tratamento de Dados.py
@@ -24,3 +24,34 @@ dados[selecao_valor]
 # Gravando o resultado no DataFrame com o inplace
 dados.dropna(subset = ['Valor'], inplace = True)
 dados
+
+# Tratamento de dados faltantes - Parte II
+filtro = dados.Condominio.isnull() 
+dados[filtro].shape[0] # 1813 valores nulos
+
+## Filtrando dados com condicionais
+selecao = (dados.Tipo == 'Apartamento') & (dados.Condominio.isnull())
+
+# Com o ~ invertemos a seleção boleana, fazendo com que o filtro seja
+# invertido e no lugar de selecionar os valores que atendem o filtro,
+# ele seleciona os valores que não atendem.
+dados = dados[~selecao]
+
+dados
+
+# Com o método fillna() atribuimos valores aos valores nulos
+dados[filtro].shape[0] # 1068 Valores nulos
+
+dados.fillna(0) # Solução 1: Substitui todos os valores nulo por 0
+
+dados
+
+dados = dados.fillna({'Condominio' : 0, 'IPTU' : 0})
+# Solução 2: A solução acima substitui os valores nulos de forma seletiva
+# definindo os valores específicos por variável. 
+
+dados.info() # Conferindo se todos os valores nulos foram removidos
+
+# Exportando o novo BD com a correção dos valores nulos
+dados.to_csv('aluguel_residencial.csv', sep = ';', index = False)
+
