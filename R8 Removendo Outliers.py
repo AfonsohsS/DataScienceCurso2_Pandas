97a0de78fffc1ede25a74a5045d3ce48d1cd4153
AfonsohsS <afonsohs@gmail.com>
2019-10-30 07:40:13 -0300

    Identificando e removendo Outliers - Parte 2

--- R8 Removendo Outliers.py
+++ R8 Removendo Outliers.py
@@ -30,3 +30,32 @@ selecao = (valor >= lower_limit) & (valor <= upper_limit)
 selected_data = dados[selecao]
 selected_data.boxplot(['Valor'])
 selected_data.hist(['Valor'])
+
+# Fazendo uma análise modular por Grupos +++++
+dados.boxplot(['Valor'], by = ['Tipo'])
+
+# Criando uma serie Valor a partir do DataFrameGroupBy
+group_type = dados.groupby('Tipo')['Valor']
+type(group_type)
+
+# Calculo dos quartis e limites (em format Series de valores)
+Q1 = group_type.quantile(.25)
+Q3 = group_type.quantile(.75)
+IIQ = Q3 - Q1 
+lower_limit = Q1 - 1.5 * IIQ
+upper_limit = Q3 + 1.5 * IIQ
+
+# Loop para percorrer os Tipos agrupados e aplicar a selecao definida
+# criando um novo DataFrame com a concatenação do Tipos e seus valores
+# redefinidos com a remoção dos outliers.
+dados_new = pd.DataFrame()
+for tipo in group_type.groups.keys():
+    eh_tipo = dados.Tipo == tipo
+    eh_dentro_limite = (dados['Valor'] >= lower_limit[tipo]) & (dados['Valor'] <= upper_limit[tipo])
+    selecao = eh_tipo & eh_dentro_limite
+    dados_selecao = dados[selecao]
+    dados_new = pd.concat([dados_new, dados_selecao])
+    
+dados_new.boxplot(['Valor'], by = ['Tipo'])
+
+dados_new.to_csv('aluguel_residencial_w_outliers.csv', sep = ';')
